24ba4807ba0de8ac004f9a1015eff221f38a4c72
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-06-26 09:41:23 +0900

    add new func FetchHomeTweetsByAccount to fetch home timeline

--- pkg/twitter/fetchTweets.go
+++ pkg/twitter/fetchTweets.go
@@ -24,3 +24,23 @@ func FetchTweetsByAccount(account string) {
 		fmt.Print("\n\n")
 	}
 }
+
+func FetchHomeTweetsByAccount(account string) {
+	values := url.Values{}
+	values.Set("screen_name", account)
+
+	tweets, err := twitterApi.GetHomeTimeline(values)
+	if err != nil {
+		panic(err)
+	}
+
+	for i, tweet := range tweets {
+		onesPlace := strconv.Itoa(i % 10)
+		coloredTweet := fmt.Sprintf(
+			"%s%s%s%s%s%s%s",
+			"\x1b[3", onesPlace, "m", tweet.User.Name, ": ", tweet.FullText, "\x1b[0m",
+		)
+		fmt.Printf(coloredTweet)
+		fmt.Print("\n\n")
+	}
+}
