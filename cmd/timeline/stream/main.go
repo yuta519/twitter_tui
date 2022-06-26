ece9ec29a1e1ef16b877164d9025c74f56ac7f5a
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-06-26 09:10:10 +0900

    change dir structure of cmd

---
+++ cmd/timeline/stream/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/yuta519/twitter_tui/pkg/twitter"
+)
+
+func main() {
+	var account string
+	fmt.Printf("Please input twitter account: ")
+	_, err := fmt.Scanln(&account)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	twitter.FetchTweetsByAccount(account)
+}
