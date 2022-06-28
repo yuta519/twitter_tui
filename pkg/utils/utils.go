c1a62e9af92f687f8faf035621e55ad3cc9398c9
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-06-28 14:03:02 +0900

    create utils dir for useful functions

---
+++ pkg/utils/utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func ColoredText(text string, index int) string {
+	coloredText := fmt.Sprintf(
+		"%s%s%s%s%s",
+		"\x1b[3", strconv.Itoa(index), "m", text, "\x1b[0m",
+	)
+	return coloredText
+}
